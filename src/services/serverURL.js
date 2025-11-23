66d91579db08de8c6701fb0a2e88cfb91d390572
Ajeshjose11 <ajeshjose11@gmail.com>
2025-11-23 12:42:56 +0530

    error resolved

--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -1,2 +1,2 @@
-// export const SERVER_URL = "https://photography-portfolio-server-2.onrender.com";
-export const SERVER_URL = "http://localhost:5173";
+export const SERVER_URL = "https://photography-portfolio-server-2.onrender.com";
+// export const SERVER_URL = "http://localhost:5173";
